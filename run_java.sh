d302232c07b9466397038f11d6cd1e15ddc32e86
Chris Smith <chrsmith@google.com>
2014-03-01 16:18:57 -0800

    Fix error due to removing the Share buttons.

--- run_java.sh
+++ run_java.sh
@@ -1,2 +1,4 @@
-../sdks/java_sdk/appengine-java-sdk-1.8.5/bin/dev_appserver.sh ./target/bloopy-fish-1
+# Manually update:
+# ../sdks/java_sdk/appengine-java-sdk-1.9.0/bin/appcfg.sh --oauth2 update ./target/bloopy-fish-1
+../sdks/java_sdk/appengine-java-sdk-1.9.0/bin/dev_appserver.sh ./target/bloopy-fish-1
 
